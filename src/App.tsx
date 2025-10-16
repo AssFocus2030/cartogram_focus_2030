13c1dfe60295039ab87bfa7c8ddc267cdcdebf27
Adrien Dupas <adriendupas@MacBook-Pro-de-Adrien.local>
2025-10-17 00:18:36 +0200

    Initial commit de mon app cartogram_FOCUS

--- src/App.tsx
+++ src/App.tsx
@@ -38,8 +38,8 @@ const styles: { [key: string]: React.CSSProperties } = {
     width: "100vw",
     height: "100vh",
     backgroundColor: "#ffffff",
-    boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
-    borderRadius: "8px",
+    
+    
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
